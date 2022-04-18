f5ec06b91da45f406c3166d760c8a1a3b50f626a
antoniodiasabc <antoniodiasabc@gmail.com>
2022-04-18 20:15:35 -0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
 app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 #app.config['MYSQL_DATABASE_HOST'] = '172.17.0.7'
 mysql.init_app(app)
 
